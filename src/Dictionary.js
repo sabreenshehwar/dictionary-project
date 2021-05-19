464afc88680d7534fa4aac118a940a3f901c1286
sabreenshehwar <76182477+sabreenshehwar@users.noreply.github.com>
2021-05-19 08:54:00 -0400

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,7 +14,6 @@ export default function Dictionary() {
     function search(event) {
         event.preventDefault();
 
-        alert(`searching for ${keyword}`);
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
 
